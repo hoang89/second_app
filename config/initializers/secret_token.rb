88f98a99c53704a24ab4c77db61573661bf629a9
Nguyen Ngoc Hoang <nguyenngochoang89@gmail.com>
2012-06-15 17:22:43 +0700

    nothing to lose

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '283d23c6e8302134e950b04ca77c364917b52b3890c255e0bca9a7ae045c2830dee26e8143eeb94ac5fc0bea1c7c485a326969c0eb8cd5d2ff7abeb688e06e9c'
